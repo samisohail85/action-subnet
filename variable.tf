76e9616a2b86f5bbf3d58b7910be122aa39d8204
samisohail85 <128447830+samisohail85@users.noreply.github.com>
2023-06-14 11:24:30 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,27 @@
+variable "project_id" {
+  type        = string
+  description = "The Project ID"
+  default = "fiery-outpost-381205"
+}
+variable "name" {
+  description = "Subnet name"
+  type = string
+  default = "sub-tf"
+}
+
+variable "vpc" {
+  description = "VPC to link the subnet to"
+  type = string
+  default = "test"
+}
+
+variable "subnet-region" {
+  type = string
+  default     = "us-central1"
+  description = "Zone associated with the subnet. Defaults to the region configured in the provider."
+}
+
+variable "ip_cidr_range" {
+  description = "IP range - format 0.0.0.0/0"
+  default = "10.0.2.0/24"
+}
