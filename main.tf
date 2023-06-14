c4678261c4528f6667f6d9e684fd4d948bc4c8ed
samisohail85 <128447830+samisohail85@users.noreply.github.com>
2023-06-14 11:32:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,5 @@
 resource "google_compute_subnetwork" "new-subnet" {
   name          = var.name
-  region        = var.subnet-region
   network       = var.vpc
   ip_cidr_range = var.ip_cidr_range
   description   = "This modules creates a subnet on target VPC"
